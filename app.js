925275b7ba2ede6c28689857893a5c662fa1b00b
tushar-h789 <tushar789imran@gmail.com>
2025-06-26 22:42:03 +0600

    otp generate 6 degit

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const authRoutes = require("./routes/auth.routes");
 const assessmentRoutes = require("./routes/assessment.routes");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/assessments", assessmentRoutes);
--- utils/generateOTP.js
+++ utils/generateOTP.js
@@ -1,5 +1,5 @@
 const generateOtp = () => {
-  return Math.floor(10000 + Math.random() * 90000).toString();
+  return Math.floor(100000 + Math.random() * 900000).toString();
 };
 
 module.exports = generateOtp;
